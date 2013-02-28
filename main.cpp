3ae6479876a7fb13e3f1f544d141a2bc9818f94e
Nathan Beckmann <beckmann@mit.edu>
2013-02-27 22:33:21 -0500

    [main] Dump each actor's marginal revenue.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include "optimize.hpp"
 #include "opec.hpp"
 #include "debug.hpp"
@@ -12,15 +13,26 @@ int main() {
     
     Market market(std::move(actors));
     
-    auto sol = solve(market);
+    auto solution = solve(market);
 
     std::cout << "\n\nDone!\n\n";
-    std::cout << "Countries: ";
+    std::cout << std::setw(20) << "Countries: ";
     for (auto actor : market.actors) {
         std::cout << actor->name << ", ";
     }
     std::cout << std::endl;
-    std::cout << sol;
-    
+    std::cout << solution << std::endl;
+
+    std::cout << "Marginal revenue (inflated):" << std::endl;
+    for (int a = 0; a < market.size(); a++) {
+        auto& actor = *market.actors[a];
+        auto q = solution.quantities.row(a);
+        auto margin = actor.marginalRevenue(solution.prices, q, market, solution.production);
+        margin -= actor.marginalCost(q);
+        margin = Market::inflate(margin);
+
+        std::cout << std::setw(18) << actor.name << ": " << margin.transpose() << std::endl;
+    }
+
     return 0;
 }
